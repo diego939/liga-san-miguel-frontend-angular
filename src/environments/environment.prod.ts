a8b2d6b0c6c15db1f801085bb0553e01cc37965f
diego939 <diegodavidalmiron1990@gmail.com>
2026-05-03 03:46:31 -0300

    fix changa enviroment back vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://liga-san-miguel-backend-nestjs-production.up.railway.app',
+  //apiUrl: 'https://liga-san-miguel-backend-nestjs-production.up.railway.app',
+  apiUrl: 'https://liga-san-miguel-backend-nestjs.vercel.app',
   appTimeZone: 'America/Argentina/Buenos_Aires',
 };
